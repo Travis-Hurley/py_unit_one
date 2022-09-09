73f76644d117a3a835142ad58ee2bd09293272e1
Travis-Hurley <hurlean@ssfs.org>
2022-09-09 14:21:48 -0400

    tried and failed

--- assignment_one.py
+++ assignment_one.py
@@ -1,1 +1,33 @@
 # remember to use comments!
+import turtle
+
+turtle.speed(0)
+for w in range(360):
+    turtle.forward(1)
+    turtle.left(1)
+
+for e in range(360):
+    turtle.right(1)
+    turtle.forward(1.5)
+
+for i in range(180):
+    turtle.forward(1)
+    turtle.left(1)
+
+
+
+turtle.forward(50)
+turtle.forward(-100)
+turtle.right(180)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(25)
+
+
+
+
+turtle.exitonclick()
