19e8fd617146451fdd5980e46277bd47befd1556
Travis-Hurley <hurlean@ssfs.org>
2022-09-14 08:36:20 -0400

    Completed simple house

--- drawing_houses.py
+++ drawing_houses.py
@@ -1,4 +1,13 @@
 import turtle
-turtle.forward(50)
+turtle.speed(50)
+for x in range(4):
+    turtle.forward(200)
+    turtle.right(90)
+
+
+for r in range(3):
+    turtle.forward(200)
+    turtle.left(120)
+
 
 turtle.exitonclick()
